3867719d96b35629468d364433a7856f5e358e4f
Igor Lesik <ilesik@tachyum.com>
2022-01-02 19:24:15 -0800

    show port width

--- src/mdbook/svmodule.rs
+++ src/mdbook/svmodule.rs
@@ -4,6 +4,7 @@
 //!
 
 use sv_parser::{parse_sv, unwrap_node, unwrap_locate, Locate, RefNode, SyntaxTree};
+use sv_parser::{PortDirection, NetType, IntegerVectorType};
 use std::collections::HashMap;
 use std::path::PathBuf;
 
@@ -65,6 +66,28 @@ fn get_identifier(node: RefNode) -> Option<Locate> {
     }
 }
 
+fn get_whole_str(
+    syntax_tree: &SyntaxTree,
+    node: &RefNode
+) -> String
+{
+    let mut s = String::new();
+
+    for subnode in node.clone().into_iter() {
+        if let RefNode::Locate(_text) = subnode {
+            let text = unwrap_locate!(subnode);
+            if let Some(text) = text {
+                let text = syntax_tree.get_str(text);
+                if let Some(text) = text {
+                    s.push_str(text);
+                }
+            }
+        }
+    }
+
+    s
+}
+
 fn print_module(
     text: &mut Vec<String>,
     file_path: &str,
@@ -80,17 +103,63 @@ fn print_module(
 
     text.push(format!("Ports: \n\n"));
     if is_ansi {
-        for node in module_node.clone().into_iter() {
-            // The type of each node is RefNode
-            match node {
-                RefNode::AnsiPortDeclaration(x) => {
-                    let id = unwrap_node!(x, PortIdentifier).unwrap();
-                    let id = unwrap_locate!(id).unwrap();
-                    let id = syntax_tree.get_str(id).unwrap();
-                    text.push(format!("- {}\n", id));
-                }
-                _ => (),
-            }
-        }
+        print_ansi_ports(text, syntax_tree, module_node);
     }
 }
+
+fn print_ansi_ports(
+    text: &mut Vec<String>,
+    syntax_tree: &SyntaxTree,
+    module_node: &RefNode
+)
+{
+    // FIXME TODO check if it can be done  without cloning
+    for node in module_node.clone().into_iter() {
+        // The type of each node is RefNode
+        match node {
+            RefNode::AnsiPortDeclaration(x) => {
+                //text.push(format!("{:?}\n", x));
+                let id = unwrap_node!(x, PortIdentifier).unwrap();
+                let id = unwrap_locate!(id).unwrap();
+                let id = syntax_tree.get_str(id).unwrap();
+                text.push(format!("- {}\n", id));
+
+                let dir = unwrap_node!(x, PortDirection);
+                let dir_str = match dir {
+                    Some(RefNode::PortDirection(PortDirection::Input(_))) => "input",
+                    Some(RefNode::PortDirection(PortDirection::Output(_))) => "output",
+                    Some(RefNode::PortDirection(PortDirection::Inout(_))) => "inout",
+                    _ => "?",
+                };
+                text.push(format!("  * direction: {}\n", dir_str));
+
+                let net_type = unwrap_node!(x, NetType);
+                match net_type {
+                    Some(RefNode::NetType(NetType::Wire(_))) =>
+                        text.push("  * type: wire\n".to_string()),
+                    _ => (),
+                }
+
+                let vnet_type = unwrap_node!(x, IntegerVectorType);
+                match vnet_type {
+                    Some(RefNode::IntegerVectorType(IntegerVectorType::Reg(_))) =>
+                        text.push("  * type: reg\n".to_string()),
+                    Some(RefNode::IntegerVectorType(IntegerVectorType::Logic(_))) =>
+                        text.push("  * type: logic\n".to_string()),
+                    Some(RefNode::IntegerVectorType(IntegerVectorType::Bit(_))) =>
+                        text.push("  * type: bit\n".to_string()),
+                    _ => (),
+                }
+
+                let width = unwrap_node!(x, PackedDimensionRange);
+                if let Some(width) = width {
+                    //text.push(format!("{:?}\n", &width));
+                    text.push(format!("  * width: {}\n", get_whole_str(syntax_tree, &width)));
+                }
+            }
+            _ => (),
+        }
+    }
+
+}
+
